043189cc6640801a10475407e1f60ca43a7131d3
lomnom <zhaoxiong.ang@gmail.com>
2022-04-15 19:24:27 +0800

    Structured code

--- Automate.py
+++ Automate.py
@@ -12,195 +12,215 @@ def fastScreenshot():
 
 pag.screenshot=fastScreenshot
 
-# locate board
-winPos=pag.locateOnScreen('TopLeft.png')[:2]
-boardPos=(winPos[0]+12,winPos[1]+74)
-bombN=(winPos[0]+18,winPos[1]+36)
-
-segments=(
-	(5, 1),
-	(1, 5),
-	(9, 5),
-	(5, 10),
-	(1, 14),
-	(10, 14),
-	(5, 19)
-)
-
-# 0000
-# 1  2 
-# 3333 
-# 4  5
-# 6666
-digits = {
-	(1,1,1,0,1,1,1): 0,
-	(0,0,1,0,0,1,0): 1,
-	(1,0,1,1,1,0,1): 2,
-	(1,0,1,1,0,1,1): 3,
-	(0,1,1,1,0,1,0): 4,
-	(1,1,0,1,0,1,1): 5,
-	(1,1,0,1,1,1,1): 6,
-	(1,0,1,0,0,1,0): 7,
-	(1,1,1,1,1,1,1): 8,
-	(1,1,1,1,0,1,1): 9,
-}
-def readSegment(x,y,screen):
-	states=[]
-	for xO,yO in segments:
-		wX,wY=xO+x,yO+y
-		states+=[int(screen.getpixel((wX,wY)) == (255,0,0))]
-	return digits[tuple(states)]
-
-cell=lambda x,y: (boardPos[0]+x*16,boardPos[1]+y*16)
-cellMid=lambda x,y: ( (boardPos[0]+x*16)+8 , (boardPos[1]+y*16)+8 )
-
-# get board dimensions
-screenshot=pag.screenshot()
-boardSize=[0,0]
-while True:
-	try:
-		possibility=screenshot.getpixel( cell(boardSize[0]+1,0) )
-	except IndexError: break
-
-	if possibility==(255, 255, 255) or possibility==(128, 128, 128): boardSize[0]+=1
-	else: break
-
-while True:
-	try: 
-		possibility=screenshot.getpixel( cell(0,boardSize[1]+1) )
-	except IndexError: break
-
-	if possibility==(255, 255, 255) or possibility==(128, 128, 128): boardSize[1]+=1
-	else: break
-
-# lookup table for cell colours
-numCols={
-	(0,0,0): "Flag",
-	(0,0,255): 1,
-	(0,128,0): 2,
-	(255,0,0): 3,
-	(0,0,128): 4,
-	(128,0,0): 5,
-	(0,128,128): 6,
-	(255,255,255): 7,
-	(128,128,128): 8
-}
-
-# solving loop
 colorStr=lambda r,g,b: f"\033[48;2;{r};{g};{b}m  \033[0m "
 
-showCProc=False
+class Board:
+	def __init__(self,screenshot,winPos=None):
+		if winPos==None:
+			self.winPos=pag.locateOnScreen('TopLeft.png')[:2]
+		else:
+			self.winPos=winPos
+		self.boardPos=(self.winPos[0]+12,self.winPos[1]+74) #top left of playfield
+		self.bombNpos=(self.winPos[0]+18,self.winPos[1]+36) #top left of bomb counter
 
-while True:
-	screenshot=pag.screenshot()
-	bombs=readSegment(bombN[0],bombN[1],screenshot)*100
-	bombs+=readSegment(bombN[0]+13,bombN[1],screenshot)*10
-	bombs+=readSegment(bombN[0]+26,bombN[1],screenshot)
+		self.refreshBoardSize(screenshot)
+		self.refreshBoardState(screenshot)
 
-	grid=[[None for _ in range(boardSize[0])] for _ in range(boardSize[1])]
+	def cell(self,x,y,offset=0): #macro to calculate coordinates of a cell
+		return ((self.boardPos[0]+x*16)+offset,(self.boardPos[1]+y*16)+offset)
 
-	try:
-		for y in range(boardSize[1]):
-			for x in range(boardSize[0]):
-				pCell=cellMid(x,y) #cell pixel position
+	def cellMid(self,x,y): #macro to calculate coordinates of middle of a cell
+		return self.cell(x,y,offset=8)
 
+	def withinBoard(self,x,y):
+		return x>=0 and y>=0 and y<self.boardSize[1] and x<self.boardSize[0]
+
+	def refreshBoardSize(self,screenshot): 
+		# get board dimensions
+		self.boardSize=[0,0]
+		while True: # keep going left till pixel of suspected cell isnt valid color
+			try:
+				cellTopLeft=screenshot.getpixel( self.cell(self.boardSize[0]+1,0) )
+			except IndexError: break # attempted cell fetch is out of screen
+
+			if cellTopLeft==(255, 255, 255) or cellTopLeft==(128, 128, 128): self.boardSize[0]+=1
+			else: break
+
+		while True:
+			try: 
+				cellTopLeft=screenshot.getpixel( self.cell(0,self.boardSize[1]+1) )
+			except IndexError: break
+
+			if cellTopLeft==(255, 255, 255) or cellTopLeft==(128, 128, 128): self.boardSize[1]+=1
+			else: break
+
+		self.boardWidth=(self.boardSize[0]*16)+20
+
+	#7-segment character parser
+	segments=(
+		(5, 1),
+		(1, 5),
+		(9, 5),
+		(5, 10),
+		(1, 14),
+		(10, 14),
+		(5, 19)
+	)
+
+	digits = {
+		(1,1,1,0,1,1,1): 0,
+		(0,0,1,0,0,1,0): 1,
+		(1,0,1,1,1,0,1): 2,
+		(1,0,1,1,0,1,1): 3,
+		(0,1,1,1,0,1,0): 4,
+		(1,1,0,1,0,1,1): 5,
+		(1,1,0,1,1,1,1): 6,
+		(1,0,1,0,0,1,0): 7,
+		(1,1,1,1,1,1,1): 8,
+		(1,1,1,1,0,1,1): 9,
+	}
+
+	def readSegment(self,x,y,screenshot):
+		states=[]
+		for xO,yO in self.segments:
+			wX,wY=xO+x,yO+y
+			states+=[int(screenshot.getpixel((wX,wY)) == (255,0,0))]
+		return self.digits[tuple(states)]
+
+	def refreshBombs(self,screenshot):
+		self.bombs=self.readSegment(self.bombNpos[0],self.bombNpos[1],screenshot)*100
+		self.bombs+=self.readSegment(self.bombNpos[0]+13,self.bombNpos[1],screenshot)*10
+		self.bombs+=self.readSegment(self.bombNpos[0]+26,self.bombNpos[1],screenshot)
+
+	# lookup table for cell colours
+	numCols={
+		(0,0,0): "Flag",
+		(0,0,255): 1,
+		(0,128,0): 2,
+		(255,0,0): 3,
+		(0,0,128): 4,
+		(128,0,0): 5,
+		(0,128,128): 6,
+		(255,255,255): 7,
+		(128,128,128): 8
+	}
+	def refreshCells(self,screenshot): #also refreshes status
+		self.state="Playing"
+		self.board=[[None for _ in range(self.boardSize[0])] for _ in range(self.boardSize[1])]
+		for y in range(self.boardSize[1]):
+			for x in range(self.boardSize[0]):
 				#red pixel means lost game
-				if screenshot.getpixel(( (boardPos[0]+x*16)+1 , (boardPos[1]+y*16)+1 ))==(255,0,0): 
-					pag.click( # click on the dead face to restart game
-						pag.locateOnScreen("Dead.png")
-					)
-					print(colorStr(164,0,0)+"Lost!")
-					raise StopIteration
-
-				# differentiate between the 2 empty cells
-				if (cellCol:=screenshot.getpixel(pCell)) == (192,192,192):
-					if screenshot.getpixel(cell(x,y)) == (255,255,255):
-						cType="Hidden"
-					else:
-						cType="Solved"
+				if screenshot.getpixel(self.cell(x,y,offset=1))==(255,0,0): 
+					self.state="Lost"
+					cType="LostMine"
+				elif screenshot.getpixel(self.cell(x,y,offset=6))==(255,255,255):
+					cType="Mine" #its a mine that shows after game finishes
 				else:
-					cType=numCols[cellCol] #number cells
+					topLeft=screenshot.getpixel(self.cellMid(x,y))
+					if topLeft == (192,192,192): # Empty cells
+						if screenshot.getpixel(self.cell(x,y)) == (255,255,255): # 2 types of empty 
+							cType="Hidden"
+						else: 
+							cType="Solved"
+					else: # cells with features
+						cType=self.numCols[topLeft] #number cells
 
-				showCProc and print(pCell,colorStr(*cellCol),f"cell({x},{y})",cType)
-				grid[y][x]=cType
-	except StopIteration:
-		continue
+				self.board[y][x]=cType
 
-	# If no more hidden tiles, game won
-	if not True in ["Hidden" in row for row in grid]:
-		pag.click( #click icon to restart game
-			pag.locateOnScreen("Won.png")
-		)
-		print(colorStr(0,255,20)+"Won!")
-		continue
+		# check for hidden tiles
+		if self.state!="Lost" and (True not in map(lambda row: "Hidden" in row,self.board)):
+			self.state="Won"
 
-	sides=((1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
+	def refreshBaseChance(self):
+		hiddenCells=sum(map(lambda row: row.count("Hidden"),self.board))
+		if hiddenCells==0:
+			self.baseChance=0
+		else:
+			self.baseChance=self.bombs/sum(map(lambda row: row.count("Hidden"),self.board))
 
-	smthHappened=False
+	def refreshBoardState(self,screenshot):
+		self.refreshBombs(screenshot)
+		self.refreshCells(screenshot)
+		self.refreshBaseChance()
 
-	for y in range(boardSize[1]):
-		for x in range(boardSize[0]):
-			cType=grid[y][x] # cell type
-			if type(cType)==int:
-				sideHidden=[]
-				sideFlags=[]
-				for oX,oY in sides: # cache cells beside current cell
-					if (y+oY)>=boardSize[1] or (x+oX)>=boardSize[0] or 0>(x+oX) or 0>(y+oY):
-						continue
-					if (cCellType:=grid[y+oY][x+oX])=="Hidden":
-						sideHidden+=[(x+oX,y+oY)]
-					elif cCellType=="Flag":
-						sideFlags+=[(x+oX,y+oY)]
+	sideOffsets=((1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
+	def neighbourIter(self,x,y):
+		for oX,oY in self.sideOffsets:
+			nX,nY=x+oX,y+oY
+			if self.withinBoard(nX,nY):
+				yield (nX,nY,self.board[nY][nX])
 
-				if (len(sideHidden)==cType and len(sideFlags)==0)\
-				    or (len(sideHidden)+len(sideFlags))==cType:
-					for bomb in sideHidden:
-						pag.click(cellMid(*bomb),button="right")
-						grid[bomb[1]][bomb[0]]="Flag"
-						smthHappened=True
-						print(colorStr(0,255,255)+f"By law of collection, {bomb} is a bomb")
-				elif len(sideFlags)==cType:
-					for bomb in sideHidden:
-						pag.click(cellMid(*bomb),button="left")
-						grid[bomb[1]][bomb[0]]="Solved"
-						smthHappened=True
-						print(colorStr(0,255,255)+f"By law of elimination, {bomb} is not a bomb!")
+	def tileIter(self,variant):
+		for y in range(self.boardSize[1]):
+			for x in range(self.boardSize[0]):
+				if type((cell:=self.board[y][x]))==variant:
+					yield (cell,x,y)
 
-	if not smthHappened:
-		print(colorStr(206,92,0)+"By law of RNG, i have to randomise.")
+	def typeIter(self,variant):
+		for y in range(self.boardSize[1]):
+			for x in range(self.boardSize[0]):
+				if (cell:=self.board[y][x])==variant:
+					yield (cell,x,y)
+
+	def sides(self,x,y): #get hidden cells beside, and flagged cells
+		sideHidden=[]
+		sideFlagN=0
+		for nX,nY,cellType in self.neighbourIter(x,y):
+			if cellType=="Hidden":
+				sideHidden+=[(nX,nY)]
+			elif cellType=="Flag":
+				sideFlagN+=1
+		return (sideHidden,sideFlagN)
+
+	def flagCell(self,x,y):
+		if not self.board[y][x]=="Flag":
+			pag.click(self.cellMid(x,y),button="right")
+			self.board[y][x]="Flag"
+
+	def unflagCell(self,x,y):
+		if not self.board[y][x]=="Flag":
+			pag.click(self.cellMid(x,y),button="right")
+			pag.click(self.cellMid(x,y),button="right")
+			self.board[y][x]="Hidden"
+
+	def exposeCell(self,x,y):
+		if self.board[y][x]=="Hidden":
+			pag.click(self.cellMid(x,y),button="left")
+
+	def newGame(self):
+		pag.click(self.winPos[0]+(self.boardWidth/2),self.winPos[1]+45)
+		screenshot=pag.screenshot()
+		self.refreshBoardState(screenshot)
+		return screenshot
+
+	# Get most likely tile to escape rng deadlock
+	def probability(self,x,y):
+		chance=self.baseChance
+		for nX,nY,sideCell in self.neighbourIter(x,y):
+			if type(sideCell)!=int:
+				continue
+
+			sidebombs=0
+			empty=0
+			for nnX,nnY,sideCellSide in self.neighbourIter(nX,nY):
+				if sideCellSide=="Flag":
+					sidebombs+=1
+				elif sideCellSide=="Hidden":
+					empty+=1
+
+			thisChance=(sideCell-sidebombs)/empty
+			chance=(chance+thisChance)/2
+
+		return chance
+
+	# get cells least likely to have bombs
+	def getBestCandidates(self):
 		possibilities=[]
-		hiddenN=sum(map(lambda row: row.count("Hidden"),grid))
+		for y in range(self.boardSize[1]):
+			for x in range(self.boardSize[0]):
+				if self.board[y][x]=="Hidden":
+					possibilities+=[[x,y,self.probability(x,y)]]
 
-		# click the cell with the least chance of being a bomb
-		for y in range(boardSize[1]):
-			for x in range(boardSize[0]):
-				if grid[y][x]=="Hidden":
-					confidence=0
-					chance=bombs/hiddenN
-					for oX,oY in sides:
-						if (cY:=y+oY)>=boardSize[1] or (cX:=x+oX)>=boardSize[0] or 0>cX or 0>cY \
-						    or type(grid[cY][cX])!=int:
-							continue
-
-						sidebombs=0
-						empty=0
-						for iX,iY in sides:
-							if (tY:=iY+cY)>=boardSize[1] or (tX:=iX+cX)>=boardSize[0] or 0>tX or 0>tY:
-								continue
-							if grid[tY][tX]=="Flag":
-								sidebombs+=1
-							elif grid[tY][tX]=="Hidden":
-								empty+=1
-
-						thisChance=(grid[cY][cX]-sidebombs)/empty
-						chance=(chance+thisChance)/2
-
-					possibilities+=[[x,y,chance]]
-
-		possibilities=sorted(
-			possibilities,
-			key=lambda candidate: candidate[2]
-		)
 
 		primeChance=possibilities[0][2]
 		for index,possibility in enumerate(possibilities):
@@ -208,15 +228,60 @@ while True:
 				possibilities=possibilities[:index]
 				break
 
-		chosen=choice(possibilities)
+		return possibilities
 
-		fraction=Fraction(chosen[2]).limit_denominator(1000000)
+	def escapeRng(self):
+		return choice(self.getBestCandidates())
+
+	def advance(self,callback=None):
+		if not self.state=="Playing":
+			callback and callback("new",self.state)
+			board.newGame()
+			return
+
+		smthHappened=False
+		for bombsAround,x,y in self.tileIter(int):
+			sideHidden,sideFlags=self.sides(x,y)
+
+			if (len(sideHidden)==bombsAround and sideFlags==0)\
+			    or (len(sideHidden)+sideFlags)==bombsAround:
+				for bomb in sideHidden:
+					self.flagCell(*bomb)
+					smthHappened=True
+					callback and callback("collection",bomb)
+
+			elif sideFlags==bombsAround:
+				for safe in sideHidden:
+					self.exposeCell(*safe)
+					smthHappened=True
+					callback and callback("elimination",safe)
+
+		if not smthHappened:
+			chosen=self.escapeRng()
+			callback and callback("rng",chosen)
+			self.exposeCell(*chosen[:2])
+
+		self.refreshBoardState(pag.screenshot())
+
+board=Board(pag.screenshot())
+
+def callback(eventType,data):
+	if eventType=="collection":
+		print(colorStr(0,255,255)+f"By law of collection, {data} is a bomb")
+	elif eventType=="elimination":
+		print(colorStr(0,255,255)+f"By law of elimination, {data} is not a bomb!")
+	elif eventType=="rng":
+		fraction=Fraction(data[2]).limit_denominator(1000000)
 		print(
 			colorStr(206,92,0)+
-			f"By law of probabilities, cell{chosen[:2]} of chance "
+			f"By law of probabilities, cell{data[:2]} of chance "
 			f"{fraction.numerator}/{fraction.denominator} is chosen!"
 		)
+	elif eventType=="new":
+		if data=="Won":
+			print(colorStr(0,255,20)+"Won!")
+		else:
+			print(colorStr(128,0,0)+"Lost...")
 
-		pag.click(cellMid(
-			*chosen[:2]
-		))
+while True:
+	board.advance(callback=callback)
